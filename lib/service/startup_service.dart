06950802b6440fcf7877f50ffdfda97c70e30782
Omar Elmanzalawy <omarmanz2002@gmail.com>
2024-10-31 23:02:30 +0300

    firebase setup

--- lib/service/startup_service.dart
+++ lib/service/startup_service.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo/Providers/tasks_provider.dart';
+import 'package:todo/firebase_options.dart';
 import 'package:todo/service/init_getit.dart';
 
 class StartupService {
 
-static void init(){
+static Future init()async{
 
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  print('Firebase Initialized');
   setupLocator();
   
 }
